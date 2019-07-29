38480cd9c56317d8e10f3539df799882e6e270a3
Peter Jones <pjones@devalot.com>
2019-07-29 10:20:14 -0700

    Update default.nix with some helpful commands

--- arduino/default.nix
+++ arduino/default.nix
@@ -8,4 +8,25 @@ pkgs.stdenv.mkDerivation {
   buildInputs = with pkgs; [
     platformio
   ];
+
+  # FIXME: This doesn't work because PlatformIO needs a network
+  # connection to download and install atmelvar.
+  buildPhase = ''
+    pio run
+  '';
+
+  installPhase = ''
+    mkdir $out
+    cp .pioenvs/micro/firmware.hex $out/
+  '';
+
+  shellHook = ''
+    upload() {
+      pio run -t upload
+    }
+
+    connect() {
+      ${pkgs.picocom}/bin/picocom --echo /dev/ttyACM0
+    }
+  '';
 }
