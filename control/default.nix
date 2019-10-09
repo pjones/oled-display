5e3598e495aa1787d46817f634966890445632d6
Peter Jones <pjones@devalot.com>
2019-10-09 12:04:39 -0700

    Update dependencies

--- control/default.nix
+++ control/default.nix
@@ -1,2 +1,14 @@
-{ pkgs ? (import <nixpkgs> {}).pkgs }:
-pkgs.haskellPackages.callPackage ./display-control.nix { }
+{ pkgs ? import <nixpkgs> { }
+}:
+
+let
+  nix-hs-src = fetchGit {
+    url = "https://code.devalot.com/open/nix-hs.git";
+    rev = "2003332a1e8e518b54e6143f9a9467a8a05abca4";
+  };
+
+  nix-hs = import "${nix-hs-src}/default.nix" { inherit pkgs; };
+
+in nix-hs {
+  cabal = ./display-control.cabal;
+}
