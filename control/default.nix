0e9cc4355742200d77ac21715cf68e884c4e9976
Peter Jones <pjones@devalot.com>
2020-04-24 11:31:56 -0700

    Update build system, use nixpkgs from NixOS 20.03

--- control/default.nix
+++ control/default.nix
@@ -1,14 +1,21 @@
-{ pkgs ? import <nixpkgs> { }
+{ sources ? import ../nix/sources.nix
+, pkgs ? import sources.nixpkgs { }
+, nix-hs ? import sources.nix-hs { inherit pkgs; }
+, ghcide ? sources.ghcide-nix
+, ormolu ? sources.ormolu
+, ghc ? "default"
 }:
 
-let
-  nix-hs-src = fetchGit {
-    url = "https://code.devalot.com/open/nix-hs.git";
-    rev = "2003332a1e8e518b54e6143f9a9467a8a05abca4";
-  };
-
-  nix-hs = import "${nix-hs-src}/default.nix" { inherit pkgs; };
-
-in nix-hs {
+nix-hs {
   cabal = ./display-control.cabal;
+  compiler = ghc;
+
+  overrides = lib: self: super: with lib; {
+    ghcide = import ghcide {};
+
+    ormolu = (import ormolu {
+      inherit (lib) pkgs;
+      ormoluCompiler = lib.compilerName;
+    }).ormolu;
+  };
 }
