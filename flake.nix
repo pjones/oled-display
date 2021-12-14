bb4e5f77c9d25f2a302ad551534432ab1269219c
Peter Jones <pjones@devalot.com>
2021-12-14 15:16:44 -0700

    flake: Add an overlay and bin output

--- flake.nix
+++ flake.nix
@@ -1,45 +1,70 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        haskell = pkgs.haskellPackages;
-      in
-      {
-        defaultPackage = self.packages.${system}.control;
+  outputs = { self, nixpkgs }:
+    let
+      # List of supported systems:
+      supportedSystems = [ "x86_64-linux" ];
 
-        packages.control = haskell.callCabal2nix "display-control" "${self}/control" {
-          relude = haskell.relude_1_0_0_1;
-          serialport =
-            let
-              src = {
-                pkg = "serialport";
-                ver = "0.5.1";
-                sha256 = "1dx5gz48kal805sl47kh8vd83gs55knqx1incm7b2xdpsyg1sb0a";
-              };
-              drv = haskell.callHackageDirect src { };
-            in
-            pkgs.haskell.lib.dontCheck drv;
-        };
+      # Function to generate a set based on supported systems:
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs supportedSystems (system: f system);
 
-        packages.firmware = import ./arduino { inherit pkgs; };
+      # Attribute set of nixpkgs for each system:
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+          haskell = pkgs.haskellPackages;
+          hlib = pkgs.haskell.lib;
+        in
+        {
+          # Full Haskell package with shared/static libraries:
+          lib = haskell.callCabal2nix "display-control" "${self}/control" {
+            relude = haskell.relude_1_0_0_1;
+            serialport =
+              let
+                src = {
+                  pkg = "serialport";
+                  ver = "0.5.1";
+                  sha256 = "1dx5gz48kal805sl47kh8vd83gs55knqx1incm7b2xdpsyg1sb0a";
+                };
+                drv = haskell.callHackageDirect src { };
+              in
+              pkgs.haskell.lib.dontCheck drv;
+          };
 
-        devShell = pkgs.mkShell {
+          # Just the inhibit-screensaver executable:
+          bin = hlib.justStaticExecutables self.packages.${system}.lib;
+
+          # The firmware to flash on the arduino:
+          firmware = import ./arduino { inherit pkgs; };
+        });
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.bin);
+
+      overlay = final: prev: {
+        pjones = (prev.pjones or { }) //
+          { display-control = self.packages.${prev.system}.bin; };
+      };
+
+      devShell = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system}; in
+        pkgs.mkShell {
           inherit (self.packages.${system}.firmware)
             ARDUINO_DIR
             ARDMK_DIR;
 
-          buildInputs = [
-            haskell.cabal-install
-            haskell.hlint
+          buildInputs = with pkgs; [
+            haskellPackages.cabal-install
+            haskellPackages.hlint
           ];
 
           inputsFrom = builtins.attrValues self.packages.${system};
-        };
-      });
+        });
+    };
 }
